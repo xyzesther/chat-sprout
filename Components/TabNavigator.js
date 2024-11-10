e7959e7c19ab3e342ad727225704fdff007fa2bb
xyzesther <zxyesther@gmail.com>
2024-11-09 17:11:38 -0800

    Refactor TabNavigator to use centralized styles for icons and tab bar

--- Components/TabNavigator.js
+++ Components/TabNavigator.js
@@ -9,7 +9,7 @@ import Entypo from '@expo/vector-icons/Entypo'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
-import { colors, iconSize } from '../styles/styles'
+import { colors, image, fontSize, spacing } from '../styles/styles'
 
 
 const Tab = createBottomTabNavigator();
@@ -17,22 +17,23 @@ const Tab = createBottomTabNavigator();
 export default function TabNavigator() {
   return (
     <Tab.Navigator
-      screenOptions={(route) => ({
+      screenOptions={({ route }) => ({
         tabBarIcon: ({ color }) => {
           if (route.name === 'Learn') {
-            return <Entypo name="chat" size={iconSize} color= {color} />
+            return <Entypo name="chat" size={image.iconImg} color= {color} />
           } else if (route.name === 'Notebook') {
-            return <MaterialCommunityIcons name="notebook-edit" size={iconSize} color={color} />
+            return <MaterialCommunityIcons name="notebook-edit" size={image.iconImg} color={color} />
           } else if (route.name === 'Explore') {
-            return <FontAwesome5 name="map-marked-alt" size={iconSize} color={color} />
+            return <FontAwesome5 name="map-marked-alt" size={image.iconImg} color={color} />
           } else if (route.name === 'Account') {
-            return <MaterialIcons name="account-circle" size={iconSize} color={color} />
+            return <MaterialIcons name="account-circle" size={image.iconImg} color={color} />
           }
         },
 
         tabBarActiveTintColor: colors.theme,
         tabBarInactiveTintColor: colors.disabled,
-        tabBarStyle: {backgroundColor: colors.background.white},
+        tabBarStyle: styles.tabBar,
+        tabBarLabelStyle: styles.tabBarLabel,
         headerStyle: {backgroundColor: colors.theme},
         headerTintColor: colors.text.primary,
       })}
@@ -45,4 +46,12 @@ export default function TabNavigator() {
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  tabBarLabel: {
+    fontSize: fontSize.tab, 
+  },
+  tabBar: {
+    backgroundColor: colors.background.white,
+    padding: spacing.md,
+  }
+})
