9ba32f78af6a4d9125bcb201dab446677c237ce9
centralperkcafe <limingyi.c@gmail.com>
2024-11-10 00:41:02 -0800

    created conversations.json

--- App.js
+++ App.js
@@ -1,34 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TabNavigator from './Components/TabNavigator';
-import { colors } from './styles/styles';
-import { createTamagui,TamaguiProvider } from 'tamagui'
-import defaultConfig from '@tamagui/config/v3'
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import TabNavigator from "./Components/TabNavigator";
+import { colors } from "./styles/styles";
+import { createTamagui, TamaguiProvider } from "tamagui";
+import defaultConfig from "@tamagui/config/v3";
 
-const config = createTamagui(defaultConfig)
+const config = createTamagui(defaultConfig);
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <TamaguiProvider config={config}>
-
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerStyle: {backgroundColor: colors.theme},
-          headerTintColor: colors.text.primary,
-        }}
-      >
-        <Stack.Screen
-          name="BottomTabNavigator"
-          component={TabNavigator}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: colors.theme },
+            headerTintColor: colors.text.primary,
+          }}
+        >
+          <Stack.Screen
+            name="BottomTabNavigator"
+            component={TabNavigator}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TamaguiProvider>
-
   );
 }
