78ca4fc40c3ab70459884881708950451fc7de87
centralperkcafe <limingyi.c@gmail.com>
2024-11-10 13:58:43 -0800

    learn screen added basic logic

--- Screens/LearnScreen.js
+++ Screens/LearnScreen.js
@@ -1,12 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import conversations from "../data/conversations";
 
 export default function LearnScreen() {
   return (
     <View>
       <Text>LearnScreen</Text>
+
+      {/* add buttons of topics  */}
+      
+      
+      {/* press the topic button leads to conversations of this topic */}
+
+      {/* create a conversation page to display - navigation? go forth and go back */}
+
     </View>
-  )
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
