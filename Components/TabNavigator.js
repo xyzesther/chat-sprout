1df88671a644d138898bc324f3bfbc16329c4488
xyzesther <zxyesther@gmail.com>
2024-11-09 16:42:12 -0800

    Create a style file and use centralized style constants in TabNavigator

--- Components/TabNavigator.js
+++ Components/TabNavigator.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import LearnScreen from '../Screens/LearnScreen'
@@ -9,6 +9,7 @@ import Entypo from '@expo/vector-icons/Entypo'
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons'
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
 import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import { colors, iconSize } from '../styles/styles'
 
 
 const Tab = createBottomTabNavigator();
@@ -19,21 +20,21 @@ export default function TabNavigator() {
       screenOptions={(route) => ({
         tabBarIcon: ({ color }) => {
           if (route.name === 'Learn') {
-            return <Entypo name="chat" size={24} color= {color} />
+            return <Entypo name="chat" size={iconSize} color= {color} />
           } else if (route.name === 'Notebook') {
-            return <MaterialCommunityIcons name="notebook-edit" size={24} color={color} />
+            return <MaterialCommunityIcons name="notebook-edit" size={iconSize} color={color} />
           } else if (route.name === 'Explore') {
-            return <FontAwesome5 name="map-marked-alt" size={24} color={color} />
+            return <FontAwesome5 name="map-marked-alt" size={iconSize} color={color} />
           } else if (route.name === 'Account') {
-            return <MaterialIcons name="account-circle" size={24} color={color} />
+            return <MaterialIcons name="account-circle" size={iconSize} color={color} />
           }
         },
 
-        tabBarActiveTintColor: '#63AE8D',
-        tabBarInactiveTintColor: 'gray',
-        tabBarStyle: {backgroundColor: 'white'},
-        headerStyle: {backgroundColor: '#63AE8D'},
-        headerTintColor: 'white',
+        tabBarActiveTintColor: colors.theme,
+        tabBarInactiveTintColor: colors.disabled,
+        tabBarStyle: {backgroundColor: colors.background.white},
+        headerStyle: {backgroundColor: colors.theme},
+        headerTintColor: colors.text.primary,
       })}
     >
       <Tab.Screen name="Learn" component={LearnScreen} />
---
+++ styles/styles.js
@@ -0,0 +1,14 @@
+export const colors = {
+  theme: '#63AE8D',
+  disabled: '#D1D1D1',
+  background: { 
+    white: '#fff',
+  },
+  text: {
+    primary: 'white',
+    secondary: '#483d8b',
+    black: 'black',
+  }
+};
+
+export const iconSize = 24;
