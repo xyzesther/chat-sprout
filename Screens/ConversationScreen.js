4eff49304e0db3ca1a64af043fcdf38f698ba5db
centralperkcafe <limingyi.c@gmail.com>
2024-11-10 16:34:03 -0800

    added buttons on learn screen to navigate to conversation screen

---
+++ Screens/ConversationScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const ConversationScreen = () => {
+  return (
+    <View>
+      <Text>ConversationScreen</Text>
+    </View>
+  );
+};
+
+export default ConversationScreen;
+
+const styles = StyleSheet.create({});
--- Screens/LearnScreen.js
+++ Screens/LearnScreen.js
@@ -1,19 +1,57 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 import React from "react";
-import conversations from "../data/conversations";
+import conversations from "../data/conversations.json";
 
-export default function LearnScreen() {
+export default function LearnScreen({ navigation }) {
+  // const navigation = useNavigation();
+  const topic1 = conversations[0].topics[0];
+  const topic2 = conversations[0].topics[1];
+  const topic3 = conversations[0].topics[2];
+  // const topic4 = conversations[1].topics[0];
+  // const topic5 = conversations[1].topics[1];
+  // const topic6 = conversations[1].topics[2];
+  // const topic7 = conversations[2].topics[0];
+  // const topic8 = conversations[2].topics[1];
+  // const topic9 = conversations[2].topics[2];
+
   return (
     <View>
       <Text>LearnScreen</Text>
 
       {/* add buttons of topics  */}
-      
-      
+      <Button
+        title={topic1.topic_name}
+        onPress={() =>
+          navigation.navigate("ConversationScreen", {
+            title: topic1.topic_name,
+            topic: topic1,
+          })
+        }
+      />
+
+      <Button
+        title={topic2.topic_name}
+        onPress={() =>
+          navigation.navigate("ConversationScreen", {
+            title: topic2.topic_name,
+            topic: topic2,
+          })
+        }
+      />
+
+      <Button
+        title={topic3.topic_name}
+        onPress={() =>
+          navigation.navigate("ConversationScreen", {
+            title: topic3.topic_name,
+            topic: topic3,
+          })
+        }
+      />
+
       {/* press the topic button leads to conversations of this topic */}
 
       {/* create a conversation page to display - navigation? go forth and go back */}
-
     </View>
   );
 }
