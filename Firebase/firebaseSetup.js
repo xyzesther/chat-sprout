cd01aec6afb44551cf55f8af3ed065e2db5c608e
xyzesther <zxyesther@gmail.com>
2024-11-16 16:39:18 -0800

    Added Firebase Storage initialization to firebaseSetup.js

--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+export const storage = getStorage(app);
